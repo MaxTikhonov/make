d61bec3fb466112f26283baedbd91dd68fd5565f
MaxTikhonov <69316402+MaxTikhonov@users.noreply.github.com>
2023-05-09 17:01:20 +0700

    made basic grid net and some part of menu

--- script/script.js
+++ script/script.js
@@ -1,8 +1,18 @@
+function changeMenuClass() {
+ document.querySelector('.menu').classList.add('menu-active');
+}
+function hideMenu() {
+ document.querySelector('.menu').classList.remove('menu-active');
+}
 function changeFontSize() {
  // const btn = document.querySelector('.btn');
  // btn.addEventListener('click', (e) => {
  //  document.body.style.setProperty('--font-s', '20px');
  // })
+ const burger = document.querySelector('.burger');
+ const menuCross = document.querySelector('.menu-cross');
+ burger.addEventListener('click', changeMenuClass);
+ menuCross.addEventListener('click', hideMenu);
  const windowW = window.innerWidth;
  if (windowW < 738) {
   document.body.style.setProperty('--line-h', '30px')
