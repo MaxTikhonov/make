baa2338126ce607eeb25ea9096420a8e4f807c06
MaxTikhonov <69316402+MaxTikhonov@users.noreply.github.com>
2023-05-12 22:08:36 +0700

    menu with adaptive fullscreen size/ changing typography in whole page/ undone falling list

--- script/script.js
+++ script/script.js
@@ -1,4 +1,77 @@
-let obj = {
+const objOfDocElements = {
+ descriptionOfStylesTextItemDesktop: '',
+ descriptionOfStylesTextItemPhone: '',
+ collectionOfH1StylesElems: [],
+ collectionOfH2StylesElems: [],
+ collectionOfH3StylesElems: [],
+ collectionOfH4StylesElems: [],
+ collectionOfPLeadDesktop: [],
+ collectionOfPBigTextDesktop: [],
+ collectionOfPCommonTextDesktop: [],
+ collectionOfPAuxiliaryTextDesktop: []
+}
+const objOfImportantVars = {
+ windowW: ''
+}
+const elemsOfDocument = {
+ burger: '',
+ menuCross: '',
+ arrOfInputs: []
+}
+function setPhoneTypographyToDocument() {
+ objOfDocElements.collectionOfH1StylesElems.forEach((item) => {
+  item.classList.add('h1-m');
+ })
+ objOfDocElements.collectionOfH2StylesElems.forEach((item) => {
+  item.classList.add('h2-m');
+ })
+ objOfDocElements.collectionOfH3StylesElems.forEach((item) => {
+  item.classList.add('h3-m');
+ })
+ objOfDocElements.collectionOfH4StylesElems.forEach((item) => {
+  item.classList.add('h4-m');
+ })
+ objOfDocElements.collectionOfPLeadDesktop.forEach((item) => {
+  item.classList.add('p-lead-m');
+ })
+ objOfDocElements.collectionOfPBigTextDesktop.forEach((item) => {
+  item.classList.add('p-big-text-m');
+ })
+ objOfDocElements.collectionOfPCommonTextDesktop.forEach((item) => {
+  item.classList.add('p-common-text-m');
+ })
+ objOfDocElements.collectionOfPAuxiliaryTextDesktop.forEach((item) => {
+  item.classList.add('p-auxiliary-m');
+ })
+}
+function setDesktopTypographyToDocument() {
+ objOfDocElements.collectionOfH1StylesElems.forEach((item) => {
+  item.classList.remove('h1-m');
+ })
+ objOfDocElements.collectionOfH2StylesElems.forEach((item) => {
+  item.classList.remove('h2-m');
+ })
+ objOfDocElements.collectionOfH3StylesElems.forEach((item) => {
+  item.classList.remove('h3-m');
+ })
+ objOfDocElements.collectionOfH4StylesElems.forEach((item) => {
+  item.classList.remove('h4-m');
+ })
+ objOfDocElements.collectionOfPLeadDesktop.forEach((item) => {
+  item.classList.remove('p-lead-m');
+ })
+ objOfDocElements.collectionOfPBigTextDesktop.forEach((item) => {
+  item.classList.remove('p-big-text-m');
+ })
+ objOfDocElements.collectionOfPCommonTextDesktop.forEach((item) => {
+  item.classList.remove('p-common-text-m');
+ })
+ objOfDocElements.collectionOfPAuxiliaryTextDesktop.forEach((item) => {
+  item.classList.remove('p-auxiliary-m');
+ })
+}
+
+const objOfInput = {
  inputValue: '',
  inFocus: false
 }
@@ -9,66 +82,79 @@ function changeMenuClass() {
 function hideMenu() {
  document.querySelector('.menu').classList.remove('menu-active');
 }
-function mouseOutHandler(e) {
- if (obj.inputValue == '' && obj.inFocus == false) {
+function mouseOutHandlerInput(e) {
+ if (objOfInput.inputValue == '' && objOfInput.inFocus == false) {
   e.target.previousElementSibling.innerHTML = '';
   e.target.classList.remove('demonstration-input-hover');
   e.target.classList.remove('demonstration-input-typing');
  }
 }
-function blurHandler(e) {
- if (obj.inputValue !== '') {
+function blurHandlerInput(e) {
+ if (objOfInput.inputValue !== '') {
   e.target.previousElementSibling.innerHTML = 'Ввод';
-  e.target.removeEventListener('mouseenter', showPlaceholderOfHover);
+  e.target.removeEventListener('mouseenter', showPlaceholderOfHoverInput);
  }
- else if (obj.inputValue == '') {
-  obj.inFocus = false;
+ else if (objOfInput.inputValue == '') {
+  objOfInput.inFocus = false;
   e.target.previousElementSibling.innerHTML = '';
   e.target.classList.remove('demonstration-input-hover');
   e.target.classList.remove('demonstration-input-typing');
-  e.target.addEventListener("mouseenter", showPlaceholderOfHover);
+  e.target.addEventListener("mouseenter", showPlaceholderOfHoverInput);
  }
 }
-function showKeyUpPlaceholder(e) {
- obj.inputValue = e.target.value;
- obj.inFocus = true;
+function showKeyUpPlaceholderInput(e) {
+ objOfInput.inputValue = e.target.value;
+ objOfInput.inFocus = true;
  e.target.classList.remove('demonstration-input-hover');
  e.target.classList.add('demonstration-input-typing');
  e.target.previousElementSibling.innerHTML = 'Ввод';
 }
-function showPlaceholderOfHover(e) {
- if (obj.inFocus == true) {
+function showPlaceholderOfHoverInput(e) {
+ if (objOfInput.inFocus == true) {
 
  }
  else {
-  console.log('зашли')
   e.target.classList.add('demonstration-input-hover');
   e.target.previousElementSibling.innerHTML = 'Ховер';
  }
 }
 function init() {
- const burger = document.querySelector('.burger');
- const menuCross = document.querySelector('.menu-cross');
- const windowW = window.innerWidth;
- const arrOfInputs = document.querySelectorAll('.demonstration-input');
- arrOfInputs.forEach(link => {
-  link.addEventListener("keyup", showKeyUpPlaceholder);
-  link.addEventListener("mouseenter", showPlaceholderOfHover);
-  link.addEventListener("focus", showKeyUpPlaceholder);
-  link.addEventListener("blur", blurHandler);
-  link.addEventListener("mouseout", mouseOutHandler);
+ objOfDocElements.descriptionOfStylesTextItemDesktop = document.querySelector('.description-of-styles-text__item-desktop');
+ objOfDocElements.descriptionOfStylesTextItemPhone = document.querySelector('.description-of-styles-text__item-phone');
+ objOfDocElements.collectionOfH1StylesElems = document.querySelectorAll('.h1-d');
+ objOfDocElements.collectionOfH2StylesElems = document.querySelectorAll('.h2-d');
+ objOfDocElements.collectionOfH3StylesElems = document.querySelectorAll('.h3-d');
+ objOfDocElements.collectionOfH4StylesElems = document.querySelectorAll('.h4-d');
+ objOfDocElements.collectionOfPLeadDesktop = document.querySelectorAll('.p-lead-d');
+ objOfDocElements.collectionOfPBigTextDesktop = document.querySelectorAll('.p-big-text-d');
+ objOfDocElements.collectionOfPCommonTextDesktop = document.querySelectorAll('.p-common-text-d');
+ objOfDocElements.collectionOfPAuxiliaryTextDesktop = document.querySelectorAll('.p-auxiliary-d');
+ objOfImportantVars.windowW = window.innerWidth;
+ elemsOfDocument.burger = document.querySelector('.burger');
+ elemsOfDocument.menuCross = document.querySelector('.menu-cross');
+ elemsOfDocument.arrOfInputs = document.querySelectorAll('.demonstration-input');
+ if (objOfImportantVars.windowW < 736) {
+  setPhoneTypographyToDocument();
+ }
+ window.onresize = function () {
+  objOfImportantVars.windowW = window.innerWidth;
+  objOfImportantVars.windowW < 736 ? setPhoneTypographyToDocument() : setDesktopTypographyToDocument();
+ }
+ arrOfInputs.forEach(item => {
+  item.addEventListener("keyup", showKeyUpPlaceholderInput);
+  item.addEventListener("mouseenter", showPlaceholderOfHoverInput);
+  item.addEventListener("focus", showKeyUpPlaceholderInput);
+  item.addEventListener("blur", blurHandlerInput);
+  item.addEventListener("mouseout", mouseOutHandlerInput);
  })
  // const btn = document.querySelector('.btn');
  // btn.addEventListener('click', (e) => {
  //  document.body.style.setProperty('--font-s', '20px');
  // })
- burger.addEventListener('click', changeMenuClass);
- menuCross.addEventListener('click', hideMenu);
- if (windowW < 738) {
-  document.body.style.setProperty('--line-h', '30px')
- }
- document.querySelectorAll("a[href^='#']").forEach(link => {
-  link.addEventListener("click", function (e) {
+ elemsOfDocument.burger.addEventListener('click', changeMenuClass);
+ elemsOfDocument.menuCross.addEventListener('click', hideMenu);
+ document.querySelectorAll("a[href^='#']").forEach(item => {
+  item.addEventListener("click", function (e) {
    e.preventDefault();
    const href = this.getAttribute("href").substring(1);
    const scrollTarget = document.getElementById(href);
@@ -81,7 +167,5 @@ function init() {
    })
   })
  })
-}
-window.onresize = function () {
 }
 window.onload = init;
